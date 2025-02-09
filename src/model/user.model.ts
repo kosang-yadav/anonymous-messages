a576555933f5e4a1ad1fcac4a3ca212ff1503264
KOSANG YADAV <kosangyadav2004@gmail.com>
2025-02-09 15:07:58 +0530

    Add Zod schemas for message, sign-in, sign-up, and verification, and implement user model with Mongoose

---
+++ src/model/user.model.ts
@@ -0,0 +1,67 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+	content: string;
+	createdAt: Date;
+}
+
+export interface User extends Document {
+	username: string;
+	email: string;
+	password: string;
+	isAcceptingMessages: boolean;
+	isVerified: boolean;
+	verifyCode: string;
+	verifyCodeExpiry: Date;
+	messages: Message[];
+	createdAt: Date;
+}
+
+const Messageschema: Schema<Message> = new Schema({
+	content: String,
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+const Userschema: Schema<User> = new Schema(
+	{
+		username: {
+			type: String,
+			required: [true, "Username is required"],
+			unique: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: [true, "Email is required"],
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: [true, "password is required"],
+		},
+		isAcceptingMessages: {
+			type: Boolean,
+			default: false,
+		},
+		isVerified: {
+			type: Boolean,
+			default: false,
+		},
+		verifyCode: String,
+		verifyCodeExpiry: {
+			type: Date,
+			default: Date.now,
+		},
+		messages: [Messageschema],
+	},
+	{ timestamps: true }
+);
+
+const UserModel =
+	(mongoose.models.User as mongoose.Model<User>) ||
+	mongoose.model<User>("User", Userschema);
+
+export default UserModel;
