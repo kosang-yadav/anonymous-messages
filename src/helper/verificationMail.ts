df087784ceeeb16e395b69d203172c8ced793c4f
KOSANG YADAV <kosangyadav2004@gmail.com>
2025-02-10 05:54:06 +0530

    Implement send email with Resend & implement a type for apiResponse

---
+++ src/helper/verificationMail.ts
@@ -0,0 +1,41 @@
+import { apiResponseSchema } from "@/type/apiResponse";
+import { VerificationMailTemplate } from "../../mail/verificationMailTemplate";
+import { resend } from "@/lib/resend";
+
+export async function sendVerificationMail(
+	username: string,
+	email: string,
+	verifyCode: string
+): Promise<apiResponseSchema> {
+	try {
+		const { data, error } = await resend.emails.send({
+			from: "Bakaonboarding@resend.dev",
+			to: email,
+			subject: "verification code for anonymous messages",
+			react: VerificationMailTemplate({ username, otp: verifyCode }),
+		});
+
+		if (error) {
+			return {
+				success: false,
+				statusCode: 500,
+				message: `resend's send mail function failed to email with error : ${error.message}`,
+				error,
+			};
+		}
+
+		return {
+			success: true,
+			statusCode: 200,
+			message: "mail sent successfully",
+			data,
+		};
+	} catch (error: any) {
+		return {
+			success: false,
+			statusCode: 500,
+			message: `our send mail function failed with error : ${error.message}`,
+			error,
+		};
+	}
+}
---
+++ src/type/apiResponse.tsx
@@ -0,0 +1,12 @@
+import { Message } from "@/model/user.model";
+
+export interface apiResponseSchema {
+    success : boolean;
+    statusCode : number;
+    message : string;
+    acceptingMessage? : boolean;
+    messages? : Array<Message>;
+    
+    data? : any;                         // required...?
+    error? : any;
+}
