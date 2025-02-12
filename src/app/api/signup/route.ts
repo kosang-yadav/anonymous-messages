254346bac9fa69882b70cb682e8a1a451f16e1a3
KOSANG YADAV <kosangyadav2004@gmail.com>
2025-02-12 15:31:54 +0530

    implement route to check unique username & debugged, tested signup, sendMail

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,50 +1,60 @@
 import { dbConnect } from "@/lib/dbConnect";
 import UserModel from "@/model/user.model";
 import { signUpSchema } from "@/schema/signUpSchema";
-import { apiResponseSchema } from "@/types/apiResponse";
 
 import { NextRequest } from "next/server";
 import bcrypt from "bcryptjs";
 import { sendVerificationMail } from "@/helper/verificationMail";
 
-export async function POST(request: NextRequest): Promise<apiResponseSchema> {
-	try {
-		const { username, email, password } = await request.json();
+export async function POST(request: NextRequest) {
+	await dbConnect();
 
+	const { username, email, password } = await request.json();
+	// console.log(username, email, password);
+	
+	try {
 		const { success, data, error } = signUpSchema.safeParse({
-			username,
 			email,
-			password,
+			username,
+			password
 		});
 
-		if (!success || error)
-			return {
-				success: false,
-				statusCode: 500,
-				message: `sign up validation failed with error : ${error.message}`,
-				error,
-			};
-
-		await dbConnect();
-
-		const hashedPassword = bcrypt.hash(password, 10);
-
-		const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
-
-		const verifyCodeExpiry = new Date();
-		// verifyCodeExpiry = verifyCodeExpiry.setHours( verifyCodeExpiry.getHours() + 1 );
-
+		if (!success)
+			return Response.json(
+		{
+			success: false,
+			message: `sign up validation failed with error : ${error.format().email?._errors || error.format().username?._errors || error.format().password?._errors}`,
+			
+			//use it to see complete error
+			// message : 'error : ' + (error)
+		},
+		{
+			status: 500,
+		}
+	);
+	
+	const hashedPassword = await bcrypt.hash(password, 10);
+	
+	const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+	
+	const verifyCodeExpiry = new Date();
+	// verifyCodeExpiry = verifyCodeExpiry.setHours( verifyCodeExpiry.getHours() + 1 );
+	
 		const user = await UserModel.findOne({ email }).select("password");
 
 		if (user) {
 			//when user is found and verified already, no need to do anything
 
 			if (user.isVerified)
-				return {
-					success: false,
-					statusCode: 500,
-					message: ` user already exists with the ${email} & verified, please try sign In`,
-				};
+				return Response.json(
+					{
+						success: false,
+						message: ` user already exists with the ${email} & verified, please try sign In`,
+					},
+					{
+						status: 500,
+					}
+				);
 
 			//when user is found but not verified, then send verification mail &update password and verifyCode
 
@@ -55,11 +65,15 @@ export async function POST(request: NextRequest): Promise<apiResponseSchema> {
 			);
 
 			if (!mailResponse.success)
-				return {
-					success: false,
-					statusCode: 500,
-					message: `failed to send verification mail to ${email}`,
-				};
+				return Response.json(
+					{
+						success: false,
+						message: `failed to send verification mail to ${email}`,
+					},
+					{
+						status: 500,
+					}
+				);
 
 			const updatedUser = await UserModel.updateOne(
 				{ email },
@@ -73,12 +87,16 @@ export async function POST(request: NextRequest): Promise<apiResponseSchema> {
 			).select("password");
 
 			if (updatedUser)
-				return {
-					success: true,
-					statusCode: 200,
-					message: `user verified & details updated successfully`,
-					data: updatedUser,
-				};
+				return Response.json(
+					{
+						success: true,
+						message: `user verified & details updated successfully`,
+						data: updatedUser,
+					},
+					{
+						status: 200,
+					}
+				);
 		}
 
 		// for new user, send verification mail & insert the user data to db
@@ -90,13 +108,15 @@ export async function POST(request: NextRequest): Promise<apiResponseSchema> {
 		);
 
 		if (!mailResponse.success)
-			return {
+			return Response.json({
 				success: false,
-				statusCode: 500,
 				message: `failed to send verification mail to ${email}`,
-			};
+			},{
+				status: 500,
+			});
 
 		const newUser = await UserModel.insertOne({
+			username,
 			email,
 			password: hashedPassword,
 			isAcceptingMessages: true,
@@ -106,25 +126,35 @@ export async function POST(request: NextRequest): Promise<apiResponseSchema> {
 			messages: [],
 		});
 
-		if (newUser)
-			return {
-				success: true,
-				statusCode: 200,
-				message: `user created successfully`,
-				data: newUser,
-			};
+		if (newUser && mailResponse.success)
+			return Response.json(
+				{
+					success: true,
+					message: `user created successfully`,
+					data: newUser,
+				},
+				{
+					status: 200,
+				}
+			);
 
-		return {
-			success: false,
-			statusCode: 500,
-			message: `failed to create new user`,
-		};
+		return Response.json(
+			{
+				success: false,
+				message: `failed to create new user`,
+			},
+			{
+				status: 500,
+			}
+		);
 	} catch (error: any) {
-		return {
-			success: false,
-			statusCode: 500,
-			message: `failed to sign up the user with error : ${error.message}`,
-			error,
-		};
+		return Response.json(
+			{
+				success: false,
+				message: `failed to sign up the user with error in catch : ${error.message}`,
+				error,
+			},
+			{ status: 500 }
+		);
 	}
 }
---
+++ src/app/api/uniqueUsername/route.ts
@@ -0,0 +1,59 @@
+import { dbConnect } from "@/lib/dbConnect";
+import UserModel from "@/model/user.model";
+import { usernameValidation } from "@/schema/signUpSchema";
+
+import { NextRequest } from "next/server";
+import { z } from "zod";
+
+const usernameQuerySchema = z.object({ username: usernameValidation });
+
+export async function GET(req: NextRequest) {
+	await dbConnect();
+
+	const { searchParams } = new URL(req.url);
+
+	const queryParams = {
+		username: searchParams.get("username"),
+	};
+
+    if(!queryParams.username) return Response.json(
+        {
+            success :false,
+            message : "username is required"
+        },
+        {status : 500}
+    )
+
+	const { success, error, data } = usernameQuerySchema.safeParse(queryParams);
+
+	if (!success)
+		return Response.json(
+			{
+				success: false,
+				message: error.format().username?._errors || "Invalid username",
+			},
+			{ status: 400 }
+		);
+
+	const username = data.username;
+    // console.log(username);
+
+	const user = await UserModel.findOne({ username, isVerified: true });
+
+	if (user)
+		return Response.json(
+			{
+				success: false,
+				message: "username is already taken",
+			},
+			{ status: 400 }
+		);
+
+	return Response.json(
+		{
+			success: true,
+			message: "username is unique",
+		},
+		{ status: 200 }
+	);
+}
